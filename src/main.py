cf30b84721e1f95c453b1894e85731fbbe927408
Mateuspv <mateuspv2003@gmail.com>
2023-10-28 19:16:52 -0300

    Finalizando projeto com patch e delete

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request, Response
 from datetime import datetime
 from pydantic import BaseModel
 
 app = FastAPI()
 
 class RegistroVeiculo(BaseModel):
+    id: int = None
     marca_veiculo: str
     placa: str
     cliente_nome: str
@@ -12,13 +13,21 @@ class RegistroVeiculo(BaseModel):
     horario_chegada: str = None
     horario_saida: str = None
 
+
 registros = []
+id_counter = 1  # Inicialize o contador de ID
+
 @app.get("/")
 def root():
     return {"message": "Status: OK"}
 
 @app.post("/registrar_veiculo/")
 def registrar_veiculo(registro: RegistroVeiculo):
+
+    global id_counter  
+    registro.id = id_counter  
+    id_counter += 1  
+
     horario_atual = datetime.now().strftime("%H:%M")
 
     for r in registros: # Valida se o veículo já saiu da manutenção
@@ -26,7 +35,7 @@ def registrar_veiculo(registro: RegistroVeiculo):
             raise HTTPException(status_code=400, detail="Esse veículo ainda está em manutenção.")
         
     for r in registros: # Valida quando já existe um registro idêntico
-        if r.dict() == registro.model_dump():
+        if r.dict(exclude={"id"}) == registro.model_dump(exclude={"id"}):
             raise HTTPException(status_code=400, detail="Esse registro já existe.")    
         
     if registro.horario_chegada == None:
@@ -37,6 +46,7 @@ def registrar_veiculo(registro: RegistroVeiculo):
 
 @app.get("/registros/")
 def listar_registros(
+    id: int = None,
     marca_veiculo: str = None,
     placa: str = None,
     cliente_nome: str = None,
@@ -47,6 +57,8 @@ def listar_registros(
     resultado = registros
 
     # Filtra os registros com base nos parâmetros passados
+    if id:
+        resultado = [r for r in resultado if r.id == id]
     if marca_veiculo:
         resultado = [r for r in resultado if r.marca_veiculo == marca_veiculo]
     if placa:
@@ -70,3 +82,47 @@ def listar_registros(
             resultado = [r for r in resultado if r.horario_saida == horario_saida]
 
     return resultado
+
+@app.patch("/atualizar_registro/{id}")
+def atualizar_registro(id: int, horario_saida: str = None):
+    registro_existe = None
+
+    # Encontra o registro existente com base no ID
+    for registro in registros:
+        if registro.id == id:
+            registro_existe = registro
+            break
+
+    if not registro_existe:
+        raise HTTPException(status_code=404, detail="Registro não encontrado")
+
+    # Atualiza a 'horario_saida' ou a hora atual
+    if horario_saida:
+        registro_existe.horario_saida = horario_saida
+    else:
+        registro_existe.horario_saida = datetime.now().strftime("%H:%M")
+
+    return registro_existe
+
+@app.delete("/excluir_registro/{id}")
+def excluir_registro(id: int):
+    global registros
+
+    registro_existe = None
+
+    # Encontra o registro existente com base no ID
+    for registro in registros:
+        if registro.id == id:
+            registro_existe= registro
+            break
+
+    # Valida se registro existe
+    if registro_existe is None:
+        raise HTTPException(status_code=404, detail="Registro não encontrado")
+
+    #Verifica se o registro contem horario de saida
+    if registro_existe.horario_saida:
+        raise HTTPException(status_code=400, detail="Este registro não pode ser excluído pois já está finalizado.")
+
+    registros = [r for r in registros if r.id != id]
+    return {"message": "Registro excluído com sucesso"}

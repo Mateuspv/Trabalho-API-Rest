f85be90ff6338f2023a29f29770aca533d9c08c0
Maeve <jtrojann@gmail.com>
2023-10-28 17:26:24 -0300

    Atualizando Post e Get

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from datetime import datetime
 from pydantic import BaseModel
 
@@ -20,11 +20,44 @@ def root():
 @app.post("/registrar_veiculo/")
 def registrar_veiculo(registro: RegistroVeiculo):
     horario_atual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    for r in registros: # Valida se o veículo já saiu da manutenção
+        if r.placa == registro.placa and not r.horario_saida:
+            raise HTTPException(status_code=400, detail="Esse veículo ainda está em manutenção.")
+        
+    for r in registros: # Valida quando já existe um registro idêntico
+        if r.dict() == registro.model_dump():
+            raise HTTPException(status_code=400, detail="Esse registro já existe.")    
+        
     if registro.horario_chegada == None:
         registro.horario_chegada = horario_atual
+
     registros.append(registro)
     return {"message": "Registro salvo com sucesso!"}
 
 @app.get("/registros/")
-def listar_registros():
-    return registros
+def listar_registros(
+    marca_veiculo: str = None,
+    placa: str = None,
+    cliente_nome: str = None,
+    mecanico_nome: str = None,
+    horario_chegada: str = None,
+    horario_saida: str = None
+):
+    resultado = registros
+
+    # Filtra os registros com base nos parâmetros passados
+    if marca_veiculo:
+        resultado = [r for r in resultado if r.marca_veiculo == marca_veiculo]
+    if placa:
+        resultado = [r for r in resultado if r.placa == placa]
+    if cliente_nome:
+        resultado = [r for r in resultado if r.cliente_nome == cliente_nome]
+    if mecanico_nome:
+        resultado = [r for r in resultado if r.mecanico_nome == mecanico_nome]
+    if horario_chegada:
+        resultado = [r for r in resultado if r.horario_chegada == horario_chegada]
+    if horario_saida:
+        resultado = [r for r in resultado if r.horario_saida == horario_saida]
+
+    return resultado
